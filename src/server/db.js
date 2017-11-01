285b0247554e1f3b84d2a4ce90e24196bfb51560
carmelo <carmelohuesca@me.com>
2017-11-01 23:09:47 +0100

    restaurar db

--- src/server/db.js
+++ src/server/db.js
@@ -1,54 +1,13 @@
-class Log {
-    static get COLOR() {
-        return {
-            RESET: '\x1b[0m',
-            BRIGHT: '\x1b[1m',
-            DIM: '\x1b[2m',
-            TRIM: '\x1b[9m',
-            UNDERSCORE: '\x1b[4m',
-            BLINK: '\x1b[5m',
-            REVERSE: '\x1b[7m',
-            HIDDEN: '\x1b[8m',
-            BLACK: '\x1b[30m',
-            RED: '\x1b[31m',
-            GREEN: '\x1b[32m',
-            YELLOW: '\x1b[33m',
-            BLUE: '\x1b[34m',
-            MAGENTA: '\x1b[35m',
-            CYAN: '\x1b[36m',
-            WHITE: '\x1b[37m',
-            BG_BLACK: '\x1b[40m',
-            BG_RED: '\x1b[41m',
-            BG_GREEN: '\x1b[42m',
-            BG_YELLOW: '\x1b[43m',
-            BG_BLUE: '\x1b[44m',
-            BG_MAGENTA: '\x1b[45m',
-            BG_CYAN: '\x1b[46m',
-            BG_WHITE: '\x1b[47m'
-        };
-    }
-
-    constructor() {
-        return this;
-    }
-
-    log() {
-        const args = Array.prototype.slice.call(arguments);
-        args.unshift(this.color || Log.COLOR.RESET);
-        args.push(Log.COLOR.RESET);
-        console.log(...args);
-        return this;
-    }
-
-    reset() {
-        delete this.color;
-        return this;
-    }
-
-    setColor(color) {
-        this.color = Log.COLOR[color.toUpperCase()];
-        return this;
-    }
-}
-
-module.exports = Log;
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+const Log = require('./Log');
+const log = new Log();
+const promise = mongoose.connect('mongodb://localhost/test', { useMongoClient: true });
+db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    log.log('================================================================');
+    log.setColor('CYAN').log('we are connected!').reset();
+    log.log('================================================================');
+});
+module.exports = db;
